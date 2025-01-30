d86e0e99696b1d7e03d796428dc02a8c19b83382
Fabricio Solis Alpizar <fabriciosolisalpizar@gmail.com>
2025-01-30 13:38:57 -0600

    feat: Angular updated to version 19

--- front-end/wss-test/src/app/app.component.ts
+++ front-end/wss-test/src/app/app.component.ts
@@ -8,11 +8,10 @@ import { MessageType, PictochatMessage, Room } from './models/room.model';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet, MatButtonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   rooms$!: Observable<Room[]>;
